9d4cdd29dac62347835bc495eda77b1c1b5848ce
M Nazim Uddin Molla <mohnazimuddin@gmail.com>
2017-10-20 02:12:32 +0600

    created ng-app, ng-model and expression

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html ng-app=""><?php // declareing angular js applicatin?>
 <head>
 	<title>Angular JS</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body>
 	<div>
-		
+		<input type="text" name="" ng-model="name"> <?php // declareing angular js model?>
+		<h3>{{ name }}</h3> <?php // declareing angular js expression?>
 	</div>
 	<script type="text/javascript">
 		
