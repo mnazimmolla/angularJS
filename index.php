dd077ee3f29e1b748c962d136b05b2c85ce99c87
M Nazim Uddin Molla <mohnazimuddin@gmail.com>
2017-10-20 02:32:59 +0600

    created angular controller and used angular controller with module

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-<html ng-app=""><?php // declareing angular js applicatin?>
+<html ng-app="myApp"><?php // declareing angular js applicatin?>
 <head>
 	<title>Angular JS</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body>
-	<div>
-		<input type="text" name="" ng-model="name"> <?php // declareing angular js model?>
-		<h3> {{ 6+6 }} </h3> <?php // using regular expression in angular?>
+	<div ng-controller="myController"> <?php // declareing angular js controller?>
+		<p>{{ name }}</p>
 	</div>
 	<script type="text/javascript">
-		
+		var data = angular.module("myApp", []); <?php // declareing angular js module?>
+		data.controller('myController', function($scope){ <?php // working with controller?>
+			$scope.name = 'Hello World';
+		});
 	</script>
 </body>
 </html>
