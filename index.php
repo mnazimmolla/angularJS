8b3a7fc2736f18f1af2ec5b8a58c07a9354ce884
M Nazim Uddin Molla <mohnazimuddin@gmail.com>
2017-10-24 07:32:22 +0600

    working with ng-repeat as array

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body>
-	<div ng-init="person=[{name: 'Hasan', age: 15}, {name: 'Riaz', age: 25}, {name: 'Shipa', age: 45}]">
-		<p ng-repeat="x in person">{{ x.name + ' ' + x.age }}</p><?php // working with ng-repeat as array object?>
+	<div ng-init="person=['Hasan', 'Jahid', 'Sumon']">
+		<p ng-repeat="x in person">{{ x }}</p><?php // working with ng-repeat as array?>
 	</div>
 </body>
 </html>
