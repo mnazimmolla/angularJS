293af9826c979c57be8b4981826237c731a4a3f2
M Nazim Uddin Molla <mohnazimuddin@gmail.com>
2017-10-24 06:26:55 +0600

    working with object ng-init

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body>
-	<div ng-init="quantity=5; cost=10">
-		{{ quantity * cost }}
-	</div> <?php // working with regular expression with ng-init?>
+	<div ng-init="person={name : 'Hasam', age : 15}">
+		{{ person.age }}
+	</div> <?php // working with object ng-init?>
 </body>
 </html>
