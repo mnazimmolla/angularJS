26560cd011eea4cd5af59a03a02c2d1b2f519ba7
M Nazim Uddin Molla <mohnazimuddin@gmail.com>
2017-10-24 06:17:37 +0600

    working with angular regular expressin

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html ng-app="myApp"><?php // declareing angular js applicatin?>
+<html ng-app=""><?php // declareing angular js applicatin?>
 <head>
 	<title>Angular JS</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body>
-
+<div>{{ 5 * 5 }}</div> <?php // working with regular expression?>
 </body>
 </html>
