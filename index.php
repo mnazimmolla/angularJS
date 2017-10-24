1bb5ceb9b91002773301b38fc8d51435c7597ff6
M Nazim Uddin Molla <mohnazimuddin@gmail.com>
2017-10-24 06:11:09 +0600

    fresh html for angular learning

--- index.php
+++ index.php
@@ -5,16 +5,6 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body>
-	<div ng-controller="myController"> <?php // declareing angular js controller?>
-		<p>{{ name }}</p>
-		<p>{{ age }}</p>
-	</div>
-	<script type="text/javascript">
-		var data = angular.module("myApp", []); <?php // declareing angular js module?>
-		data.controller('myController', function($scope){ <?php // working with controller?>
-			$scope.name = 'Hello World';
-			$scope.age = 25;
-		});
-	</script>
+
 </body>
 </html>
