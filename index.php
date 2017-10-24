96850e34fc067781709d15f134a75b9dd7c8abac
M Nazim Uddin Molla <mohnazimuddin@gmail.com>
2017-10-24 06:37:36 +0600

    working with object array in ng-init

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body>
-	<div ng-init="person=[5, 20, 52, 85]">
-		<p>{{ person[1] }}</p><?php // working with array in ng-init?>
+	<div ng-init="person=[{name: 'Hasan', age: 15}, {name: 'Riaz', age: 25}, {name: 'Shipa', age: 45}]">
+		<p>{{ person[1].name }}</p><?php // working with object array in ng-init?>
 	</div>
 </body>
 </html>
