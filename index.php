8bade0492fff9fbc49e6a45214f97dc6de5b3e10
M Nazim Uddin Molla <mohnazimuddin@gmail.com>
2017-10-31 03:28:02 +0600

    validated user input

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html ng-app=""><?php // declareing angular js applicatin?>
+<html><?php // declareing angular js applicatin?>
 <head>
 	<title>Angular JS</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body>
-	<div ng-init="person=['Hasan', 'Jahid', 'Sumon']">
-		<p ng-repeat="x in person">{{ x }}</p><?php // working with ng-repeat as array?>
-	</div>
+	<form ng-app="" name="myform">
+		<input type="email" name="name" ng-model="myName">
+		<span ng-show="myform.name.$error.email">Ivallid Email Address</span>
+	</form>
 </body>
 </html>
